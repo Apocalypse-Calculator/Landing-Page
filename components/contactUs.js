f2e7b0ca307986133a01ea53b739e7e31013b089
emilygracekz <57361805+emilygracekz@users.noreply.github.com>
2020-04-30 08:25:14 -0500

    getform added not functioning

--- components/contactUs.js
+++ components/contactUs.js
@@ -1,25 +1,46 @@
-import { up } from 'styled-breakpoints';
-import styled from 'styled-components';
-import { theme } from '../styles/theme';
+import { up } from "styled-breakpoints";
+import styled from "styled-components";
+import { theme } from "../styles/theme";
 
-const ContactUs = () => (
-  <Container id='contact-us'>
-    <h2>
-      <img src='/images/contact.png' alt='' />
-      Contact Us
-    </h2>
-    <p>
-      Have a question or want to know more? <br /> Our volunteer team is here to
-      help, so please do fill in the contact form below!
-    </p>
-    <form>
-      <input type='email' name='email' placeholder='Email Address' required/>
-      <input type='text' name='subject' placeholder='Subject' required/>
-      <textarea rows={4} name='comments' placeholder='Comments' required/>
-      <input type='submit' value='Contact us' />
-    </form>
-  </Container>
-);
+const ContactUs = () => {
+  function onSubmit(e) {
+    e.preventDefault();
+    window.location.href = url;
+
+    window.fetch("getform.io/forms/16946", {
+      method: "POST",
+      redirect: "follow",
+      body: JSON.stringify(""), // e.g. {email, name}
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // .then(display);
+  }
+
+  return (
+    <Container id="contact-us">
+      <h2>
+        <img src="/images/contact.png" alt="" />
+        Contact Us
+      </h2>
+      <p>
+        Have a question or want to know more? <br /> Our volunteer team is here
+        to help, so please do fill in the contact form below!
+      </p>
+      <form
+        action="https://getform.io/f/bdda47df-89fa-45e6-ba16-4b5c03e05a51"
+        method="POST"
+        name="contact-form"
+      >
+        <input type="email" name="email" placeholder="Email Address" required />
+        <input type="text" name="subject" placeholder="Subject" required />
+        <textarea rows={4} name="comments" placeholder="Comments" required />
+        <input type="submit" value="Contact us" />
+      </form>
+    </Container>
+  );
+};
 
 export default ContactUs;
 
@@ -42,10 +63,10 @@ const Container = styled.section`
   }
 
   p {
-    ${up('tablet')} {
+    ${up("tablet")} {
       text-align: center;
       font-size: 1.5em;
-      letter-spacing: .3px;
+      letter-spacing: 0.3px;
     }
   }
 
@@ -57,7 +78,7 @@ const Container = styled.section`
     max-width: 1000px;
     margin: 0 auto;
 
-    ${up('tablet')} {
+    ${up("tablet")} {
       max-width: 60ch;
       margin: 0 auto;
     }
@@ -82,7 +103,7 @@ const Container = styled.section`
       }
     }
 
-    input[type='submit'] {
+    input[type="submit"] {
       background: ${theme.colors.primary};
       color: white;
       padding: 0.5em;
